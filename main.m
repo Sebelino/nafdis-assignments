a90ce660d2ef99e28636eefd1fcd05d8837f352a
Ville Sebastian Olsson <seo@kth.se>
2025-01-19 15:59:42 +0100

    refactor: Silence output

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ V = [
     2 2 2002 2004;
     3 2 2001 2003;
     4 7 7005 7012;
-]
+];
 
 [Q,R,errs] = gsces(V);
 
@@ -13,14 +13,14 @@ errs(3)
 plot(1:minsz(V), errs)
 set(gca,'YScale','log')
 
-V = load_mat_hw1(1000,100)
+V = load_mat_hw1(1000,100);
 
 [Q,R,errs] = gsces(V)
 
 plot(1:minsz(V), errs)
 set(gca,'YScale','log')
 
-[Q,R,errs] = gsceg(V)
+[Q,R,errs] = gsceg(V);
 
 plot(1:minsz(V), errs)
 set(gca,'YScale','log')
