62bc27e60cf063879a1a50463080705d8f4bc6a1
Ville Sebastian Olsson <seo@kth.se>
2025-01-21 13:05:36 +0100

    feat: Scale + export figure

--- hw1_1b.m
+++ hw1_1b.m
@@ -11,5 +11,12 @@ V = [
 
 disp(['3rd error: ', num2str(errs(3))])
 
+figure;
 plot(1:minsz(V), errs)
 set(gca,'YScale','log')
+axis square;
+set(gca,'YLim',[1e-20, 1e5])
+xlabel('j')
+ylabel('$||A_j||$', 'Interpreter', 'latex')
+
+exportgraphics(gcf, 'hw1_1b.png');
