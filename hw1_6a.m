c61283683c009337aa716dc8b8a5b49b073eac7d
Ville Sebastian Olsson <seo@kth.se>
2025-01-28 19:03:43 +0100

    fix: Remove weird limit from plot

--- hw1_6a.m
+++ hw1_6a.m
@@ -7,7 +7,6 @@ p = 20;
 f = figure('Visible','off');
 semilogy(1:p, errs)
 axis square;
-set(gca,'YLim',[1e-20, 1e5])
 xlabel('j')
 ylabel('$||A_j||$', 'Interpreter', 'latex')
 saveas(f, [mfilename,'.png']);
