65da8c9935754bf751a8a48a41f99ee387565208
Ville Sebastian Olsson <seo@kth.se>
2025-01-29 21:26:55 +0100

    feat: Solve 5e propertly

--- hw1_5e.m
+++ hw1_5e.m
@@ -6,6 +6,42 @@ diff = A-u*v';
 
 tol = norm(diff, 2);
 
-tic; svd(A,0) ; toc
+%tols=[0.5*tol,tol,tol*2,tol*4,tol*8];
+tols = 1e4 * (1:4);
 
-tic; approxsvd(A,tol) ; toc
+P=3; % number of samples
+
+T1=zeros(length(tols),P);
+T2=zeros(length(tols),P);
+
+for p=1:P
+    for k=1:length(tols)
+        tol=tols(k);
+
+        % Method one
+        tic
+        svd(A,0);
+        T1(k,p)=toc;
+
+        % Method two
+        tic
+        svdapprox(A, tol);
+        T2(k,p)=toc;
+    end
+end
+
+disp(['Tolerance levels: ', num2str(tols)])
+disp(['Naive SVD:        ', num2str(t1')])
+disp(['Partial SVD:      ', num2str(t2')])
+
+t1=mean(T1,2); % Plot the mean of the runs
+t2=mean(T2,2);
+clf;
+f = figure('Visible','off');
+plot(tols,t1,tols,t2);
+set(gca, 'XScale', 'log');
+legend('t1','t2');
+xlabel('tol');
+ylabel('CPU time (s)');
+saveas(f, [mfilename,'.png']);
+f.Visible = 'on';
