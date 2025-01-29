2a209c8564f133e1e6c933c5e993e73e2888fe32
Ville Sebastian Olsson <seo@kth.se>
2025-01-29 21:26:41 +0100

    perf: Optimize svdapprox

--- svdapprox.m
+++ svdapprox.m
@@ -1,24 +1,27 @@
 function [X,k]=svdapprox(B, tol)
-R = [];
 A = B;
 [m, n] = size(B);
 p = min(m,n);
-Q = [];
+R = zeros(p,n); % Preallocate
+Q = zeros(m,p); % Preallocate
 
 for j = 1:p
     [~, i] = max(vecnorm(A, 2, 1));
     q = A(:,i) / norm(A(:,i));
     r = q' * A;
-    Q = [Q,q];
-    R = [R ; r];
+    Q(:,j) = q;
+    R(j,:) = r;
     A = A - q*r;
-    t = norm(A,2);
+    t = norm(A,'fro');
 
     if t < tol
         k = j;
         break;
     end
 end
+
+Q = Q(:,1:k); % Strip unfilled zeros
+R = R(1:k,:); % Strip unfilled zeros
 
 [Uhat,S,V] = svd(R,'econ');
 U = Q*Uhat;
