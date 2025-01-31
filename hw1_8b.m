ee75d8b7430f25510c1d8fb5e6de676ecad11835
Ville Sebastian Olsson <seo@kth.se>
2025-01-31 08:29:02 +0100

    feat: Make plot in 8b

--- hw1_8b.m
+++ hw1_8b.m
@@ -4,4 +4,18 @@ A = item3;
 [U,S,V] = svd(A);
 [C,Z] = ID_col(A,784);
 
-zalando_plot(C(:,1));
+imageSize=28;
+imageCount = 3;
+niter = 2*imageCount;
+
+f = figure('Visible','on');
+
+for i = 1:niter
+    subplot(3,2,i);
+    img = zalandoImage(C(:,i));
+    imshow(img,'InitialMagnification', 1000);
+    img = zalandoImage(U(:,i));
+    imshow(img,'InitialMagnification', 1000);
+    drawnow;
+end
+saveas(f, [mfilename,'.png']);
