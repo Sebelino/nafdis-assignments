9a6b77108bcbd68d77adb105879fe244e9a6ccd1
Ville Sebastian Olsson <seo@kth.se>
2025-01-31 08:01:00 +0100

    feat: Finish 7b

--- hw1_7b.m
+++ hw1_7b.m
@@ -2,7 +2,26 @@ A = readImages('roundabout_snapshots');
 k = 5;
 s = 15;
 
-tic; randomSvd(A, k, s); toc;
-%disp(['Elapsed time for s=15: ',num2str(elapsed)]);
+P=10;
+T1=zeros(P,1);
+T2=zeros(P,1);
 
-tic; gsceb(A, @greedyidx, k); toc;
+for p=1:P
+    % Method one
+    tic
+    gsceb(A, @greedyidx, k);
+    T1(p,1)=toc;
+
+    % Method two
+    tic
+    randomSvd(A, k, s);
+    T2(p,1)=toc;
+
+    fprintf('Iteration %d/%d: %g, %g\n',p,P,T1(p,1),T2(p,1))
+end
+
+t1=mean(T1); % Plot the mean of the runs
+t2=mean(T2);
+
+disp(['Greedy Algorithm 1: ', num2str(t1)])
+disp(['Randomized SVD:     ', num2str(t2)])
