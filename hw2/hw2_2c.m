071454208e1ce0bd557d2f0a4e7c2476a1489979
Ville Sebastian Olsson <seo@kth.se>
2025-02-01 17:18:30 +0100

    refactor: Break out to fn

--- hw2/hw2_2c.m
+++ hw2/hw2_2c.m
@@ -1,10 +1,7 @@
 A = makeDataMatrix(3);
 ee = 2.5;
 Dist = makeDistances(A);
-
-W=ones(size(Dist));
-W(Dist>ee)=0; % Set too far away to
-W(Dist==0)=0;  % Avoid an edge to itself
+W = makeWeights(Dist,ee);
 
 D=zeros(size(A,1));
 for i=1:size(A,1)
