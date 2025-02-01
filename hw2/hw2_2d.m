4fb4ace89acd595bd4810a0a60a4c7dbd6b593a5
Ville Sebastian Olsson <seo@kth.se>
2025-02-01 16:59:42 +0100

    refactor: Break out to fn

--- hw2/hw2_2d.m
+++ hw2/hw2_2d.m
@@ -1,11 +1,4 @@
-n0=5; p=3;
-randn('seed',0);
-c=3.1;
-A1=randn(n0,p)+c*[1,0,0];
-A2=randn(n0,p)+c*[0,1,0];
-A3=randn(n0,p)+c*[0,0,1];
-A4=[0,0,0];
-A=[A1;A2;A3;A4];
+A = myDataMatrix(3.1);
 
 Dist=zeros(size(A,1));
 for i=1:size(A,1)
