c51a7f0bc537ab4f607cf113691de6c1bb09a04e
Ville Sebastian Olsson <seo@kth.se>
2025-02-02 16:57:49 +0100

    refactor: Extract to fn

---
+++ hw2/plotClustersMap.m
@@ -0,0 +1,24 @@
+function clusters = plotClustersMap(timeseries,x_coords,y_coords,k)
+Dist = makeDistances(timeseries);
+k = 3;
+W = makeKnnWeights(Dist, k);
+D = makeDegreeMatrix(W);
+L = D - W;
+clusterCount = 7;
+%[U, ~] = eigs(L, clusterCount, 'smallestabs');
+[U, ~] = eigs(L, clusterCount, 0.0001);
+clusters = kmeans(U, clusterCount);
+
+f = figure('Visible','off','Units', 'pixels', 'Position', [0,0,200,800]);
+t = tiledlayout(4,2);
+t.Padding = 'compact';
+t.TileSpacing = 'compact';
+for i = 1:clusterCount
+    bengali = imread('misc/bengali_map.png');
+    nexttile;
+    imshow(bengali); hold on;
+    indices = find(clusters == i);
+    plot(y_coords(indices),x_coords(indices),'r*')
+end
+f.Visible = 'on';
+end
