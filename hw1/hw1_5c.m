a46a057f7e20522d8da7bc50b1517e7c70c2edd3
Ville Sebastian Olsson <seo@kth.se>
2025-02-04 17:40:59 +0100

    feat: Upper casing

--- hw1/hw1_5c.m
+++ hw1/hw1_5c.m
@@ -7,4 +7,4 @@ relative_error = norm(diff, 2)/norm(A,2);
 
 disp(['Rank of u*v'': ', num2str(rank(u*v'))])
 disp(['||A-uv''||: ', num2str(norm(diff, 2))])
-disp(['relative error of ||A-uv''||/||A||: ', num2str(relative_error)])
+disp(['Relative error of ||A-uv''||/||A||: ', num2str(relative_error)])
