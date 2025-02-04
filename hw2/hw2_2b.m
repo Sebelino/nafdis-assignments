d7d77d659042f1e8c84b727db4be37fa22514ec8
Ville Sebastian Olsson <seo@kth.se>
2025-02-04 08:51:51 +0100

    fix: Correct number of arguments

--- hw2/hw2_2b.m
+++ hw2/hw2_2b.m
@@ -2,7 +2,7 @@ A = makeDataMatrix(2);
 ee = 2.5;
 Dist = makeDistances(A);
 W = makeWeights(Dist,ee);
-D = makeDegreeMatrix(A,W);
+D = makeDegreeMatrix(W);
 
 L = D-W;
 
--- hw2/hw2_2c.m
+++ hw2/hw2_2c.m
@@ -2,7 +2,7 @@ A = makeDataMatrix(3);
 ee = 2.5;
 Dist = makeDistances(A);
 W = makeWeights(Dist,ee);
-D = makeDegreeMatrix(A,W);
+D = makeDegreeMatrix(W);
 
 n=size(W,1);
 z=exp(((1:n)-1)*2i*pi/n);
