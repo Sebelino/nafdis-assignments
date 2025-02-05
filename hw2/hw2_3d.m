a58c9c412f8388e7374447a75866d25ea2aa38fa
Ville Sebastian Olsson <seo@kth.se>
2025-02-05 17:35:56 +0100

    refactor: Extract to var

--- hw2/hw2_3d.m
+++ hw2/hw2_3d.m
@@ -1,4 +1,5 @@
 load('bengali_cleanup.mat');
 
 outImagePath = [mfilename,'.png'];
-plotClustersMap(timeseries,x_coords,y_coords,3,outImagePath);
+k = 3;
+plotClustersMap(timeseries,x_coords,y_coords,k,outImagePath);
--- hw2/hw2_3e.m
+++ hw2/hw2_3e.m
@@ -1,4 +1,5 @@
 load('bengali_cleanup.mat');
 
 outImagePath = [mfilename,'.png'];
-plotClustersMap(timeseries,x_coords,y_coords,2,outImagePath);
+k = 2;
+plotClustersMap(timeseries,x_coords,y_coords,k,outImagePath);
