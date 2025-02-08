871076124b16f784f3fd3b8ea8ec3e7fe7d9fc08
Ville Sebastian Olsson <seo@kth.se>
2025-02-08 15:19:57 +0100

    refactor: Extract to fn

--- hw2/hw2_4d.m
+++ hw2/hw2_4d.m
@@ -1,28 +1,7 @@
-load('zalando_clustering.mat');
+load('zalando_clustering.mat', 'items', 'correct');
 
-n = 1000;
-w = ones(n);
-D = buildDistance(items, w);
-
-W = zeros(n);
-alpha = 0.5;
-for i = 1:n
-    sgma = std(D(:,i));
-    for j = 1:n
-        W(i,j) = exp(-alpha*D(i,j)^2/sgma^2);
-    end
-end
-W = W-diag(diag(W));
-W = (W+W')/2; % We want an undirected graph
-
-De = makeDegreeMatrix(W);
-L = De - W;
-
-[V,~] = eig(L);
-v2 = V(:,2);
-
+v2 = spectralClustering(items);
 tau = median(v2);
-
 predicted = (v2<tau)+1;
 
 [truePositives,~] = size(find(predicted-correct==0));
