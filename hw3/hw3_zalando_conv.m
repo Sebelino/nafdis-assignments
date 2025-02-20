1761ac6ff2ecb98208ae7606e0b841367d58c3b3
Ville Sebastian Olsson <seo@kth.se>
2025-02-20 10:38:41 +0100

    refactor: Softcode misclassified index finding

--- hw3/hw3_zalando_conv.m
+++ hw3/hw3_zalando_conv.m
@@ -21,12 +21,17 @@ I=kmeans(items_conv',2); % kmeans returns 1 or 2. But we don't know
                          % which one corresponds to 4 and 9. Try the best
 guess1=(I==1)*4+(I==2)*9;
 guess2=(I==1)*9+(I==2)*4;
-% How many incorrectly classified images?
-min(sum(guess1 ~= correct ), sum(guess2 ~= correct ))
 
-incorrectlyClassifiedIndices = find(guess2 ~= correct);
+if sum(guess1 ~= correct) < p/2
+    guess = guess1;
+else
+    guess = guess2;
+end
 
-firstIncorrectlyClassifiedIndex = 60;
+incorrectlyClassifiedIndices = find(guess ~= correct);
+firstIncorrectlyClassifiedIndex = incorrectlyClassifiedIndices(1);
+
+zalando_plot(items(:,firstIncorrectlyClassifiedIndex))
 
 %% Circulant matrix multiplication
 
