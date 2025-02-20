605d9f6fddabf6aa50c18ef2f666a1d28ba48cf1
Ville Sebastian Olsson <seo@kth.se>
2025-02-20 10:22:37 +0100

    feat: Working on 2b a bit

--- hw3/hw3_2b.m
+++ hw3/hw3_2b.m
@@ -2,19 +2,22 @@ fname = 'hw3_terrible_sound_with_hidden_message.ogg';
 
 [y, Fs] = audioread(fname);
 
-n = 2^11;
+n = 2^12; % cannot exceed 2^17
+tic;
 y = y(1:n);
 
 Fn = dftMatrix(n);
 
+tic;
 yf = Fn * y;
+toc;
 
 freqs = (0:n-1) * (Fs / n);
 noise_indices = 1:100:n;
 
 yfa = abs(yf);
 
-plot(freqs, yfa);
+%plot(freqs, yfa);
 
 annoyingIndices = find(yfa > 100);
 
@@ -23,3 +26,5 @@ yf(annoyingIndices) = 0;
 y2 = 1/n * inv(Fn) * yf;
 
 %audiowrite('cleaned_audio.wav', y2, Fs);
+
+toc;
