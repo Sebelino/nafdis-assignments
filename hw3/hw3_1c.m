d6addaa7d923c064128b660883ddb47ded2e0ca9
sil-pixel <silpa2408@gmail.com>
2025-02-22 18:22:04 +0100

    q1 changes

--- hw3/hw3_1c.m
+++ hw3/hw3_1c.m
@@ -1,29 +1,39 @@
-p = 12;
+p_values = [10,12,14];
 
 sampleCount = 3;
-T1=zeros(p,sampleCount);
-T2=zeros(p,sampleCount);
+f = figure('Visible','off','Units', 'pixels', 'Position', [0, 0, 1200, 600]);
+t = tiledlayout(1,3);
+t.Padding = 'compact';
+t.TileSpacing = 'compact';
 
-for s = 1:sampleCount
-    for j = 1:p
-        b = rand(2.^j,1);
-        Fn = dftMatrix(numel(b));
+for p = p_values
+    T1=zeros(p,sampleCount);
+    T2=zeros(p,sampleCount);
 
-        tic;
-        Fn*b;
-        T1(j,s) = toc;
+    for s = 1:sampleCount
+        for j = 1:p
+            b = rand(2.^j,1);
+            Fn = dftMatrix(numel(b));
 
-        tic;
-        fftx(b);
-        T2(j,s) = toc;
+            tic;
+            Fn*b;
+            T1(j,s) = toc;
+
+            tic;
+            fftx(b);
+            T2(j,s) = toc;
+        end
     end
+
+    t1 = mean(T1,2);
+    t2 = mean(T2,2);
+
+    nexttile;
+    plot(2.^(1:p),t1,2.^(1:p),t2);
+    legend('naive','FFT');
+    xlabel('n');
+    ylabel('CPU time');
+    title(['CPU Time Comparison (p = ', num2str(p), ')']);
+    exportgraphics(t,[mfilename,'.png']);
+    f.Visible = 'on';
 end
-
-t1 = mean(T1,2);
-t2 = mean(T2,2);
-
-clf;
-plot(2.^(1:p),t1,2.^(1:p),t2);
-legend('naive','FFT');
-xlabel('n');
-ylabel('CPU time');
