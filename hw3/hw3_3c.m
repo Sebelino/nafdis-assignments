e77a4ad5a87a2049b6620e30ab2e23e664b4a030
sil-pixel <silpa2408@gmail.com>
2025-02-25 01:12:43 +0100

    time loop changes

--- hw3/hw3_3c.m
+++ hw3/hw3_3c.m
@@ -1,4 +1,4 @@
-ns = 1000:500:2000;
+ns = [1000,1500,2000,4000,8000];
 randn('seed',0);
 
 for n = ns
@@ -15,4 +15,5 @@ for n = ns
 
     tic; [V,D]=eigs(f_naive,n,3,'sr'); t1 = toc;
     tic; [V,D]=eigs(f_smat,n,3,'sr'); t2 = toc;
+    disp(['For n=', num2str(n), ', t1=', num2str(t1), ', t2=', num2str(t2)]);
 end
--- hw3/smatrix_skeleton.m
+++ hw3/smatrix_skeleton.m
@@ -1,5 +1,5 @@
 % Compute a semiseparable matrix of size n times n
-n=6000;
+n=8000;
 randn('seed',0);
 v=randn(n,1);
 D=diag(randn(n,1));
@@ -12,21 +12,21 @@ stopsize=8; % Do not subdivide intervals smaller than this.
 % improve performance
 [index_dict,child_dict]=bisection_tree(1,n, stopsize);
 
-display('Computed tree');
+%display('Computed tree');
 
 % HODLR matrix vector multiply
-disp('HODLR')
+%disp('HODLR')
 tic
 b1 = matvec_hodlr(x, A, v, index_dict, child_dict);
 tt1=toc;
 
 % Naive matrix vector multiply
-disp('Naive')
+%disp('Naive')
 tic
 b2 = matvec_naive(x, A);
 tt2=toc;
 
-disp(['Timing HODLR: ' num2str(tt1)])
-disp(['Timing naive: ' num2str(tt2)])
+disp(['Timing HODLR for n=', num2str(n), ' is : ' num2str(tt1)])
+disp(['Timing naive for n=', num2str(n), ' is : ' num2str(tt2)])
 
-norm(b1-b2) % should be zero
+disp(['2-norm difference between Naive and HODLR : ', num2str(norm(b1-b2))]) % should be zero
